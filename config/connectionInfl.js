702153393c871cd4824a668dd6fff22e254edaeb
Scott Kumor <scott.kumor1212@gmail.com>
2019-12-29 14:23:25 -0600

    connections and orm

---
+++ config/connectionInfl.js
@@ -0,0 +1,28 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connectionInfl;
+
+if (process.env.JAWSDB_URL) {
+  connectionInfl = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
+  connectionInfl = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "youllneverguessit",
+    database: "influencers_db"
+  });
+}
+
+// Make connection.
+connectionInfl.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connectionInfl.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connectionInfl;

0fd1e2794cadfbe8427e4cbd3d6b752980984ba9
Oriana Dentici <oriana.c.dentici@gmail.com>
2020-01-04 10:59:32 -0600

    influencer db updates

--- db/influencer.sql
+++ db/influencer.sql
@@ -7,5 +7,7 @@ CREATE TABLE influencers
 	name varchar(300) NOT NULL,
 	ig_handle varchar(300) NOT NULL, 
     followers int NOT NULL,
+	brand varchar(300) NOT NULL,
+	date_posted date NOT NULL, 
 	PRIMARY KEY (id)
 );

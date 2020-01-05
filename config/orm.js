d97535c91268027bbb4d7f93cf361fc2ebc6dad1
Scott Kumor <scott.kumor1212@gmail.com>
2020-01-05 14:50:26 -0600

    added inputs and create methods

--- config/orm.js
+++ config/orm.js
@@ -50,6 +50,26 @@ var orm = {
       cb(result);
     });
   },
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
 };
 
 // Export the orm object for the model (cat.js).

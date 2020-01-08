3cdab851dae0dd4eabadc41e4f02751eb544571d
Oriana Dentici <oriana.c.dentici@gmail.com>
2020-01-07 19:54:09 -0600

    db updates and new table addition

--- db/instagram.sql
+++ db/instagram.sql
@@ -4,18 +4,25 @@ USE instagram_db;
 CREATE TABLE brands
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(300) NOT NULL,
+	brand varchar(300) NOT NULL,
 	ticker_symbol varchar(50) NOT NULL, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE influencer_post
+(
+	id int NOT NULL AUTO_INCREMENT,
+	inf_name varchar(300) NOT NULL,
+	ig_handle varchar(300) NOT NULL, 
+	brand, varchar(300) NOT NULL,
+	date_posted date NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE influencers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(300) NOT NULL,
-	ig_handle varchar(300) NOT NULL, 
-    followers int NOT NULL,
-	brand varchar(300) NOT NULL,
-	date_posted date NOT NULL, 
+	inf_name varchar(300) NOT NULL,
+	ig_handle varchar(300) NOT NULL,
 	PRIMARY KEY (id)
 );
